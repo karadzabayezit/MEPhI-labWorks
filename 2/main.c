b677844118417337277b27245b2ad7e95d9e1236
karadzabayezit <bayezitkarajaew@gmail.com>
2024-10-20 19:23:15 +0300

    fix: main & main2

--- 2/main.c
+++ 2/main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long double inputX();
-int inputLimit();
+int inputX(long double *x);
+int inputLimit(int *limit);
 
 int main() {
-	long double x = inputX();
-	int limit = inputLimit();
-	
+	long double x;
+	if(inputX(&x) == EOF) {
+		return 0;
+	};
+	int limit;
+	if(inputLimit(&limit) == EOF){
+		return 0;
+	};
+
 	long double numInDegree = 1;
 	long double factorial = 1;
 	long double res;
-		
-	for(int i = 0; i <= limit ; i++) {
-		int degree = -1;
-		if(i % 2 == 0){
-			degree = 1;
-		};
+
+	for(int i = 0; i < limit ; i++) {
+		int degree = (i % 2 == 0) ? 1 : -1;
+		printf("%d", degree);
 
 		if(i > 0) {
 			numInDegree *= x * x;
@@ -27,11 +31,11 @@ int main() {
 		printf("fac: %Lf\n", factorial);
 		printf("\n");
 
-		long double devided = numInDegree / factorial;	
-		
+		long double devided = numInDegree / factorial;
+
 		res += (long double) degree * devided;
 	}
-	
+
 
 	printf("RES: %.32Lf\n", res);
 
@@ -39,49 +43,47 @@ int main() {
 };
 
 
-long double inputX() {
-	long double x;
+int inputX(long double *x) {
 	char inputValue[20];
 
-	
-	while (1) {	
+
+	while (1) {
 		printf("Gimme x: ");
 		if(fgets(inputValue, sizeof(inputValue), stdin) == NULL){
 			printf("\n!!!!!!!    EOF    !!!!!!!!!!!!!!\n");
-			exit(0);
+			return EOF;
 		};
 
 		char *notNum;
 
-		x = strtold(inputValue, &notNum);
+		*x = strtold(inputValue, &notNum);
 
 		if(notNum == inputValue || *notNum != '\n') {
 			printf("Incorrect. ");
 		} else {
-			return x;
-		}		
+			return 0;
+		}
 	}
 }
 
-int inputLimit() {
-	int limit;
+int inputLimit(int *limit) {
 	char inputValue[20];
-	
-	while (1) {	
+
+	while (1) {
 		printf("Gimme limit: ");
 		if(fgets(inputValue, sizeof(inputValue), stdin) == NULL){
 			printf("\n!!!!!!!    EOF    !!!!!!!!!!!!!!\n");
-			exit(0);
+			return EOF;
 		};
 
 		char *notNum;
 
-		limit = strtol(inputValue, &notNum, 10);
+		*limit = strtol(inputValue, &notNum, 10);
 
 		if(notNum == inputValue || *notNum != '\n') {
 			printf("Incorrect. ");
 		} else {
-			return limit;
-		}		
+			return 0;
+		}
 	}
 }
